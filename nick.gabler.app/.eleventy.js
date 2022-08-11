9a738089a03fcc54c9edc382a33a423e2c272950
ngabler <nick@gabler.app>
2022-08-11 00:11:40 -0700

    adding headers

--- nick.gabler.app/.eleventy.js
+++ nick.gabler.app/.eleventy.js
@@ -2,4 +2,5 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("fonts");
   eleventyConfig.addPassthroughCopy("images");
+  eleventyConfig.addPassthroughCopy("_headers");
 };
