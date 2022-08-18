16250e86297ac060d34f5951699fc1daa35b526f
ngabler <nick@gabler.app>
2022-08-17 17:07:49 -0700

    Performance tuning.

--- nick.gabler.app/.eleventy.js
+++ nick.gabler.app/.eleventy.js
@@ -1,5 +1,4 @@
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("fonts");
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("_headers");
