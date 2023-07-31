ed123b943d0bc5ed137aa9c550d7dd62a6ed309a
Nick Gabler <91996558+ngabler@users.noreply.github.com>
2023-07-31 15:51:15 -0700

    Update .eleventy.js

--- nick.gabler.app/.eleventy.js
+++ nick.gabler.app/.eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("_headers");
+  eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("fonts");
   eleventyConfig.addPassthroughCopy("images");
-  eleventyConfig.addPassthroughCopy("_headers");
   eleventyConfig.addPassthroughCopy("scripts");
 };
