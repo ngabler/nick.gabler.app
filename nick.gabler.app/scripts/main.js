bc7b6f81e15692aa3dce256960d44b08c057b8f5
Nick Gabler <91996558+ngabler@users.noreply.github.com>
2024-02-26 20:15:42 -0800

    Update main.js

--- nick.gabler.app/scripts/main.js
+++ nick.gabler.app/scripts/main.js
@@ -1,10 +1,10 @@
 window.onload = function () {
-    gsap.to('#title', {
+    gsap.from('#title', {
         opacity: 0,
         duration: 1,
         onComplete: () => {
             // After the title animation completes, animate each link
-            gsap.to('#social-links a', {
+            gsap.from('#social-links a', {
                 opacity: 0, // Animate opacity to 1
                 duration: 0.5, // Each animation takes 0.5 seconds
                 stagger: 0.2, // Delay each subsequent animation by 0.2 seconds
