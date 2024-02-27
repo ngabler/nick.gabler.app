9bf3f893b6f7cecdf61a136707bb3d609c91fd85
Nick Gabler <91996558+ngabler@users.noreply.github.com>
2024-02-26 22:29:49 -0800

    Update main.js

--- nick.gabler.app/scripts/main.js
+++ nick.gabler.app/scripts/main.js
@@ -8,6 +8,7 @@ window.onload = function () {
     tl.to('#social-links a', {
         opacity: 1,
         duration: 0.5,
+        stagger: 0.2,
         ease: "expoScale(0.5,7,power1.inOut)",
     });
 };
