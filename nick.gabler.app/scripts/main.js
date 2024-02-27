06debf1b7b413ac4844d8ce921c55afdfc82462f
Nick Gabler <91996558+ngabler@users.noreply.github.com>
2024-02-26 23:53:08 -0800

    Update main.js

--- nick.gabler.app/scripts/main.js
+++ nick.gabler.app/scripts/main.js
@@ -13,45 +13,48 @@ window.onload = function () {
     });
     document.querySelectorAll('#social-links a').forEach(link => {
         link.style.position = 'relative'; // Necessary for positioning the canvas correctly
+
         link.addEventListener('mouseover', function () {
-            let canvas = document.createElement('canvas');
-            canvas.width = this.offsetWidth;
-            let height = 20; // Adjust height as needed
-            canvas.height = height;
-            canvas.style.position = 'absolute';
-            canvas.style.left = '0';
-            canvas.style.top = `${this.offsetHeight - 30}px`;
-            this.appendChild(canvas);
+            // Check if the canvas already exists
+            if (!this.querySelector('canvas')) {
+                let canvas = document.createElement('canvas');
+                canvas.width = this.offsetWidth;
+                let height = 20; // Adjust height as needed
+                canvas.height = height;
+                canvas.style.position = 'absolute';
+                canvas.style.left = '0';
+                canvas.style.top = `${this.offsetHeight - 30}px`; // Adjust the position as needed
+                this.appendChild(canvas);
 
-            let rc = rough.canvas(canvas);
+                let rc = rough.canvas(canvas);
 
-            let lastWidth = 0; // Variable to store the last width at which the line was drawn
-            const redrawThreshold = 10; // Set the threshold for redrawing the line, e.g., 10 pixels
+                let lastWidth = 0; // Variable to store the last width at which the line was drawn
+                const redrawThreshold = 10; // Set the threshold for redrawing the line, e.g., 10 pixels
 
-            function drawLine(newWidth) {
-                // Check if the change in width is enough to redraw the line
-                if (Math.abs(newWidth - lastWidth) >= redrawThreshold) {
-                    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
-                    rc.line(0, 10, newWidth, 10, {
-                        stroke: '#F8F8F8', strokeWidth: 4, roughness: 2.5
-                    });
-                    lastWidth = newWidth; // Update the lastWidth with the newWidth
+                function drawLine(newWidth) {
+                    // Check if the change in width is enough to redraw the line
+                    if (Math.abs(newWidth - lastWidth) >= redrawThreshold) {
+                        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+                        rc.line(0, 10, newWidth, 10, {
+                            stroke: '#F8F8F8', strokeWidth: 4, roughness: 2.5
+                        });
+                        lastWidth = newWidth; // Update the lastWidth with the newWidth
+                    }
                 }
+
+                gsap.fromTo({width: 0}, {
+                    width: 0
+                }, {
+                    width: canvas.offsetWidth,
+                    duration: 1,
+                    ease: "none",
+                    onUpdate: function () {
+                        let currentWidth = this.targets()[0].width;
+                        drawLine(currentWidth); // Redraw the line conditionally
+                    },
+                    yoyo: true
+                });
             }
-
-            gsap.fromTo({width: 0}, {
-                width: 0
-            }, {
-                width: canvas.offsetWidth,
-                duration: 1,
-                ease: "none",
-                onUpdate: function () {
-                    let currentWidth = this.targets()[0].width;
-                    drawLine(currentWidth); // Redraw the line conditionally
-                },
-                yoyo: true
-            });
-
         });
 
         link.addEventListener('mouseout', function () {
