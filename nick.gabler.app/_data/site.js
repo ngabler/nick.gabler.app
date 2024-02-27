c1a6cce5c25d7bb6201df480e8817d3e17af9d20
Nick Gabler <91996558+ngabler@users.noreply.github.com>
2024-02-26 19:07:22 -0800

    Create site.js

---
+++ nick.gabler.app/_data/site.js
@@ -0,0 +1,12 @@
+module.exports = {
+  firstname: "NICK",
+  lastname: "GABLER",
+  website: "nick.gabler.app",
+  linklist: {
+    Contact: "mailto:nick@gabler.app",
+    GitHub: "https://github.com/ngabler",
+    Instagram: "https://www.instagram.com/swappiness0/",
+    Linkedin: "https://www.linkedin.com/in/nick-gabler-505b9453/",
+    𝕏: "https://x.com/swappiness0"
+  }
+};
