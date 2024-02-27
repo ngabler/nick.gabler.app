fb53c1077882e24247f9f494b7ab24878a4469d6
Nick Gabler <91996558+ngabler@users.noreply.github.com>
2024-02-26 22:07:14 -0800

    Update main.js

--- nick.gabler.app/scripts/main.js
+++ nick.gabler.app/scripts/main.js
@@ -1,15 +1,15 @@
 window.onload = function () {
-    gsap.to('#title', {
+    let tl = gsap.timeline();
+    tl.to('#title', {
         opacity: 1,
         duration: 1,
-        ease: 'power1.inOut',
-        onComplete: () => {
-            gsap.to('#social-links a', {
-                opacity: 1,
-                duration: 0.5,
-                stagger: 0.2,
-                ease: 'power1.inOut',
-            });
-        }
-    })
+        ease: 'power1.inOut'
+    });
+    tl.to('#social-links a', {
+        opacity: 1,
+        duration: 0.5,
+        stagger: 0.2,
+        ease: 'power1.inOut'
+    });
 };
+
