68726a7de39830dcd5389811fd722655a9ea8f04
Nick Gabler <91996558+ngabler@users.noreply.github.com>
2024-02-27 17:05:01 -0800

    Update main.js

--- nick.gabler.app/scripts/main.js
+++ nick.gabler.app/scripts/main.js
@@ -52,7 +52,7 @@ window.onload = function () {
 
         rc.rectangle(newX, padding + borderPadding, newWidth, boxAnim.height - borderPadding * 2, {
             fill: fillStyle,
-            fillStyle: 'cross-hatch',
+            fillStyle: 'zig-zag',
             hachureAngle: 45,
             hachureGap: 10,
             fillWeight: 1.5,
