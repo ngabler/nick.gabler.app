f7d847f6aaa98cd36ddc08ce32edb739a709e868
Nick Gabler <91996558+ngabler@users.noreply.github.com>
2024-02-27 23:24:57 -0800

    Update main.js

--- nick.gabler.app/scripts/main.js
+++ nick.gabler.app/scripts/main.js
@@ -5,7 +5,7 @@ window.onload = function () {
     let linkDuration = 0.5;
     let extraPadding = 30;
     let lastBoxUpdate = 0;
-    let boxUpdateInterval = 33.33;
+    let boxUpdateInterval = 41.67;
     let animationInterval;
 
     let title = document.getElementById('title');
@@ -72,7 +72,7 @@ window.onload = function () {
             if (animationInterval) clearInterval(animationInterval); // Clear existing interval if any
             animationInterval = setInterval(function () {
                 drawRectangle(canvas, boxAnim.width, boxAnim.height, boxAnim.opacity);
-            }, 33.33); // Redraw every 250ms
+            }, 41.67); // Redraw every 250ms
         }
     })
         .to('#title', { opacity: 1, duration: duration, ease: 'expo.out' })
